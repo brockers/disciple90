f5787648bb3b363e49ca3f25a0b6a7d481b9df0f
Bobby Rockers <rrockers@gmail.com>
2025-01-18 14:27:23 -0600

    Initial data object of the checklist.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 // Start date of 2025 disciple 90
 const start = new Date(2025, 0, 20);
-const listContainer = document.getElementById("list-container");
-const viewdate = document.getElementById("view-date");
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thusday", "Friday", "Saturday"];
-const ninetyDays = Array(90).fill(start.getTime()).map( (v,k) => new Date(v + (k * (60 * 60 * 24 * 1000)) ));
+// Default everything to unchecked for all days
+const unchecked = Boolean(false);
 const results = {
-	"sl" : 1, "cs" : 1, "ex" : 0,
-	"pc" : 0, "tv" : 0, "al" : 0,
-	"sn" : 0, "mu" : 0, "pu" : 1,
-	"fa" : 0
+	"sl" : unchecked, "cs" : unchecked, "ex" : unchecked,
+	"pc" : unchecked, "tv" : unchecked, "al" : unchecked,
+	"sn" : unchecked, "mu" : unchecked, "pu" : unchecked,
+	"fa" : unchecked
 }
+const ninetyDays = Array(90).fill(start.getTime()).map( (v,k) => {
+    return {
+        "date" : new Date(v + (k * (60 * 60 * 24 * 1000)) ),
+        "results" : results
+    }
+});
+
+const listContainer = document.getElementById("list-container");
+const viewdate = document.getElementById("view-date");
 
 function setResults(r){
 	Object.entries(r).forEach( ([n,v]) => {
 		const element = document.getElementById(n);
-		if(v===1){
+		if(v){
 			element.classList.add("checked");
 		} else {
 			element.classList.remove("checked");
@@ -24,4 +32,12 @@ function setResults(r){
 }
 
 viewdate.value = days[start.getDay()] + " " + months[start.getMonth()] + " " + start.getDate() + ", 2025";
+
+listContainer.addEventListener("click", (e) => {
+    if( results.hasOwnProperty(e.target.id) ){
+        results[e.target.id] = results[e.target.id] ? false : true;
+        setResults(results);
+    }
+});
+
 setResults(results);
